9e6944f94e352a3e4c0dc39dc0eb35499f3f9f34
ailan <ailan@ailan-X556UQ>
2017-11-03 21:34:47 +0800

    update account model

--- theWaster/account/models.py
+++ theWaster/account/models.py
@@ -6,9 +6,10 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, related_name='userProfile')
     name = models.CharField(max_length=128) # 姓名
     sex = models.CharField(max_length=128) # 性別
-    birthdate = models.DateField(null=True, blank=True) # 生日
-    introduction = models.TextField() #自我介紹
-    lavel = models.IntegerField() #經驗
-    
+    birthdate = models.DateField() # 生日
+    introduction = models.TextField(blank=True, null=True) # *自我介紹
+    lavel = models.IntegerField() # 經驗
+    emailVerifyCode = models.CharField(max_length=128) # Email驗證碼
+    isEmailVerified = models.BooleanField(default=False) # Email驗證
     def __str__(self):
         return self.name + '(' + self.user.username + ')'

63ca24dc5d35f9ff736349f2bc270f3491cacc85
ailan <ailan@ailan-X556UQ>
2017-11-25 12:48:15 +0800

    想要catch edition id

--- theWaster/main/views.py
+++ theWaster/main/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 
+from edition.models import Sort, Edition
 
 
 def main(request): 
     context = {} 
     return render(request, 'main/main.html', context) 
 
+
 def templates(request): 
     return render(request, 'main/templates.html') 
 
-    

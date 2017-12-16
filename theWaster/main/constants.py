a2b48e630bde2770ff305caa19dc7b20d900009e
ailan <ailan@ailan-X556UQ>
2017-12-16 23:22:43 +0800

    pagination

--- theWaster/main/constants.py
+++ theWaster/main/constants.py
@@ -1,5 +1,6 @@
 from edition.models import Sort, Edition
 
+NUM_ITEMS_PER_PAGE = 5
 
 SORT = Sort.objects.filter(disabled=False)
 EDITION = Edition.objects.filter(disabled=False)
--- theWaster/main/views.py
+++ theWaster/main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
-from edition.models import Sort, Edition
-
+from main.constants import NUM_ITEMS_PER_PAGE
 
 def main(request): 
     context = {} 
@@ -11,3 +11,27 @@ def main(request):
 def templates(request): 
     return render(request, 'main/templates.html') 
 
+
+def pagination(request, queryset, numItemsPerPage=NUM_ITEMS_PER_PAGE):   
+    paginator = Paginator(queryset, numItemsPerPage)
+    page = request.GET.get('page')
+    try:
+        queryset = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        queryset = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        queryset = paginator.page(paginator.num_pages)
+    
+    fullPath = request.get_full_path()
+    if fullPath.find('?') < 0:    # No search terms: add a question mark so that 'page' may be appended
+        fullPath += '?'
+    else:    # There are search terms
+        index = fullPath.find('page')
+        if index < 0:        # No 'page': add an '&' so that 'page' may be appended
+            fullPath += '&'
+        else:                # There is 'page': remove 'page' info
+            fullPath = fullPath[:index]
+            
+    return {'queryset':queryset, 'paginator':paginator, 'fullPath':fullPath}
